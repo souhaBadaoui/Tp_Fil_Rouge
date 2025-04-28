1292d3f8590a53860e02680266ed5ac5a101fd47
souhaBadaoui <souha.badaoui@etu.univ-lyon1.fr>
2025-04-28 10:12:39 +0200

    Delete film_reco_project directory

--- film_reco_project/backend/crud.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from backend.models import Movie
-from backend.database import SessionLocal
-
-def get_all_movies():
-    db = SessionLocal()
-    movies = db.query(Movie).all()
-    db.close()
-    return movies
